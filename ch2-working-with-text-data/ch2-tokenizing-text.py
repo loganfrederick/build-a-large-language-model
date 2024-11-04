aeded74703a4cd8aba0d9eb88988f5af11d5f60d
Logan Frederick <loganfrederick@gmail.com>
2024-11-04 18:37:40 -0500

    Tokenizing Text

--- ch2-working-with-text-data/ch2-tokenizing-text.py
+++ ch2-working-with-text-data/ch2-tokenizing-text.py
@@ -3,3 +3,35 @@ with open("the-verdict.txt", "r", encoding="utf-8") as f:
 
 print("Total number of character:", len(raw_text))
 print(raw_text[:99])
+
+# Different tokenizations
+import re
+text = "Hello, world. This, is a test."
+result = re.split(r'([,.]|\s)', text)
+print(result)
+
+result = [item for item in result if item.strip()]
+print(result)
+
+text = "Hello, world. Is this-- a test?"
+result = re.split(r'([,.:;?_!"()\"]|--|\s)',text)
+print(result)
+
+# Apply tokenization to Edith Wharton The Verdict"
+
+preprocessed = re.split(r'([,.:;?_!"()\"]|--|\s)',raw_text)
+preprocessed = [item.strip() for item in preprocessed if item.strip()]
+# NOTE: The book says the bleow should output 4690 but I got 4519
+print(len(preprocessed))
+print(preprocessed[:30])
+
+# List all unique tokens and sort htem alphabetically to determine vocab size
+all_words = sorted(set(preprocessed))
+vocab_size = len(all_words)
+print(vocab_size)
+
+vocab = {token:integer for integer,token in enumerate(all_words)}
+for i, item in enumerate(vocab.items()):
+    print(item)
+    if i>=50:
+        break
