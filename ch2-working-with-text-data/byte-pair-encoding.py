f1533e34579970c77b056d74bcb8046503086cbb
Logan Frederick <loganfrederick@gmail.com>
2024-11-07 13:36:08 -0500

    Byte Pair Encoding

---
+++ ch2-working-with-text-data/byte-pair-encoding.py
@@ -0,0 +1,15 @@
+#from importlib.metadata import version
+import tiktoken
+
+# NOTE: get_encoding needs an internet connection, failed at coffee shop
+tokenizer = tiktoken.get_encoding("gpt2")
+
+text = (
+    "Hello, do you like tea? <|endoftext|> In the sunlit terraces"
+    "of someunknownPlace."
+)
+integers = tokenizer.encode(text, allowed_special={"<|endoftext|>"})
+print(integers)
+
+strings = tokenizer.decode(integers)
+print(strings)
